8ca6409f858db5ff346fbba35392c7eed4e7bb70
Ronnie James <55567463+RonnieJames98@users.noreply.github.com>
2022-02-07 09:39:43 -0800

    Update 2/7/2022
    Added comment on index,php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <?php
         
         ?>
+        <!-- Links the form action to upload.php. Uses the post method to store this data on the server. -->
         <form action = "upload.php" method="post" enctype="multipart/form-data">
             <input type = "file" name ="file">
             <button type = "submit" name="submit">SUBMIT</button>
